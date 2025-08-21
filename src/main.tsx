f693eda2f6c855f4023a6913d049e63052f60727
camiexemplar <camilyvitoriaof@gmail.com>
2025-08-21 20:37:43 -0300

    feature/content

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { Content } from './assets/components/content.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <h1>olá</h1>
   </StrictMode>,
 )
