8aa884444299d707e27d8f599227c256831f4cc6
camiexemplar <camilyvitoriaof@gmail.com>
2025-08-21 20:51:28 -0300

    feature:header

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Content } from './assets/components/content'
+import { Header } from './assets/components/header'
 
 function App() {
   return (
     <>
     <h1>olá</h1>
     <Content />
+    <Header
+    title='guai'
+    description='oxi'
+    />
+    <Header
+    title='nada'
+    description='guei'
+    value='0'
+    />
+
     </>
   )
 }
